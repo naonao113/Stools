e039b45648a2fd348f434901035fd46c4ebacf24
maqiang <>
2020-12-16 19:26:19 +0800

    qdi212

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -2,15 +2,26 @@
     
     class News extends CI_Controller{
         
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->model('news_model');
+
+        }
+
         public function view($page="news"){
             if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
                // show_404();
                //echo "2222";
             }
             $data['title']=ucfirst($page);
+            $data['conent']=$this->news_model->get_news();
             $this->load->view("template/header",$data);
             $this->load->view("pages/".$page,$data);
             $this->load->view("template/footer",$data);
 
+        }
+        public function index(){
+            
         }
     }
--- application/models/News_model.php
+++ application/models/News_model.php
@@ -10,6 +10,7 @@
         public function get_news($slug=false){
             if($slug===false){
                 $query=$this->db-get('news');
+                var_dump($query);
                 return $query->result_array();
             }
             $query=$this->db->get_where('news',array('slug'=>$slug));
